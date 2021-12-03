011892deafa52211a3d2714c7bccb76a477d7760
zlatanned <shahiakshay31@gmail.com>
2021-12-03 22:52:26 +0530

    💄 : UI and styles added for homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
 function App() {
   return (
-    <div>Hello World</div>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
