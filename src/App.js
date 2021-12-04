715db13fe1b593b5991cad740e9af29656c401b3
zlatanned <shahiakshay31@gmail.com>
2021-12-04 23:25:13 +0530

    manifest updated after logo deletion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/hats' element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
