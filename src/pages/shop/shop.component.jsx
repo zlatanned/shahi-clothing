3122cd4d4314bba283a4874a9ae149dbb0fffe81
zlatanned <shahiakshay31@gmail.com>
2021-12-05 20:24:17 +0530

    💄 : UI and styles added for Shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data.js';
 
-class shopPage extends React.Component {
-    constructor(props) {
-        super(props);
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
 
-    render() {
-        return <div>SHOP PAGE</div>;
-    }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default shopPage;
+export default ShopPage;
