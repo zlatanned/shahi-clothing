80a63b16b2c3843e5d7ff4af4b4e0b6725b46cce
zlatanned <shahiakshay31@gmail.com>
2021-12-08 15:44:12 +0530

    google sign-in moved out of form tag

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -53,11 +53,14 @@ class SignIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit"> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              Sign in with Google
-            </CustomButton>
           </div>
+          <br />
         </form>
+        <div className="buttons">
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            Sign in with Google
+          </CustomButton>
+        </div>
       </div>
     );
   }
